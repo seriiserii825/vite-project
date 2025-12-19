9daf60a467add4b5a73e3a36f9c886410936ef9d
serii <seriiburduja@gmail.com>
2025-12-19 20:35:15 +0200

    feat: connect typescript

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 // vite.config.js
-import { resolve } from 'path';
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
+  base: "./",
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, "partials"),
